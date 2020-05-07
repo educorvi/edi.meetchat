84c9929b123d2e9a6a7b1af1c46e03e546e7feed
Neferin <neferin42@gmail.com>
2020-05-07 15:05:25 +0200

    testcredentials aus repository herausgezogen

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import credentials from "@/../testcredentials.json"
 
 Vue.use(Vuex)
 
@@ -11,11 +12,11 @@ export default new Vuex.Store({
     chats: [
       {
         name: "Große Gruppe",
-        url: "https://testuser:testabc123@couch.kraeks.de/classroomchat_547a3ea1533a45b69221fc0d9e156b18"
+        url: `https://${credentials.username}:${credentials.password}@couch.kraeks.de/classroomchat_547a3ea1533a45b69221fc0d9e156b18`
       },
       {
         name: "Kleine Gruppe",
-        url: "https://testuser:testabc123@couch.kraeks.de/classroomchat_cab8d5d8529047af882e2258e5698d27"
+        url: `https://${credentials.username}:${credentials.password}@couch.kraeks.de/classroomchat_cab8d5d8529047af882e2258e5698d27`
       }
     ],
     //Der aktuell ausgewählte/geöffnete Chat
